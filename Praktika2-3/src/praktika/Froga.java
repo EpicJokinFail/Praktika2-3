343c813a34c134565f9a9688b76a9d0b4c605098
EpicJokinFail <joaquin.pb@hotmail.es>
2017-09-28 17:20:03 +0200

    Bigarren bertsioa

--- Praktika2-3/src/praktika/Froga.java
+++ Praktika2-3/src/praktika/Froga.java
@@ -4,6 +4,7 @@ public class Froga {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Esto es una prueba");
+		System.out.println("Esto es una prueba. 2.0");
+		
 	}
 }
